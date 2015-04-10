0dd1ad6200a229eb859540b62c38cb5fd547a6e0
Alex Rakoczy <arakoczy@gmail.com>
2015-04-10 13:21:27 -0400

    Connect to multiple databases

--- heroku-aws-rds-test.rb
+++ heroku-aws-rds-test.rb
@@ -1,6 +1,5 @@
 class HerokuAwsRdsTest
-  def self::pg_connection
-    uri = URI.parse(ENV["DATABASE_URL"])
+  def self::pg_connection(uri)
     options = {
       host: uri.host,
       port: uri.port,
@@ -13,13 +12,23 @@ class HerokuAwsRdsTest
     PG::Connection.new(options)
   end
 
+  def self::amazon_rds_connection
+    uri = URI.parse(ENV["AMAZON_RDS_POSTGRES_URL"])
+    @amazon_rds_connection ||= pg_connection(uri)
+  end
+
+  def self::heroku_postgres_connection
+    uri = URI.parse(ENV["HEROKU_POSTGRESQL_PURPLE_URL"])
+    @heroku_postgres_connection ||= pg_connection(uri)
+  end
+
   class App < Sinatra::Base
     get '/' do
       require 'benchmark'
       output = ""
       output << Benchmark.bm do |x|
-        x.report("Query current time") { output << "#{HerokuAwsRdsTest.pg_connection.exec('select NOW() AS current_time').first["current_time"]}<br/>" }
-      end.first.format("%n: user: %u system: %y total: %t real: %r")
+        x.report("Query current time") { output << "#{HerokuAwsRdsTest.amazon_rds_connection.exec('select NOW() AS current_time').first["current_time"]}<br/>" }
+      end.map {|report| report.format("%n: user: %u system: %y total: %t real: %r") }.join("<br/>")
       output
     end
   end
